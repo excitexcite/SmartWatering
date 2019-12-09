5892f41e7ace140fae179500e6e3db268b714911
frezzi <49375343+frezzi@users.noreply.github.com>
2019-12-09 21:15:01 +0200

    Update SmartWatering.ino
    relay work test

--- SmartWatering/SmartWatering.ino
+++ SmartWatering/SmartWatering.ino
@@ -1,1 +1,12 @@
-//project initialization
+//relay work test
+#define relay = 8;
+
+void setup() {
+  pinMode(relay, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(relay, HIGH);
+  delay(5000);
+  digitalWrite(relay, LOW);
+}
