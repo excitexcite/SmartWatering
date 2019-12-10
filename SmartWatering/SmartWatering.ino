c3c0d39f231f879942c2fdb99f4465483cb1e16b
NeKit-Papakin <papakinnik@gmail.com>
2019-12-10 08:35:02 +0300

    Update SmartWatering.ino
    LED diod support test

--- SmartWatering/SmartWatering.ino
+++ SmartWatering/SmartWatering.ino
@@ -1,29 +1,35 @@
-//moisture sensor work test
+//diod support test
 #define relay = 8;
 #define sensorPower = 2;
 #define sensorControl = A0;
+#define redDiod = 7;
+#define greenDiod = 13; 
 int moisture_value;
 
 void setup() {
   Serial.begin(9600);
   pinMode(sensorPower, OUTPUT);
   pinMode(relay, OUTPUT);
+  pinMode(redDiod, OUTPUT);
+  pinMode(greenDiod, OUTPUT); 
 }
 
 void loop() {
   digitalWrite(sensorPower, HIGH);
   //get moisture value
   moisture_value = analogRead(sensorControl);
-  //output moisutre value to serial console
   Serial.print(moisture_value);
   Serial.print("\n");
-  //watering plant is the earth is dry
+  //red light until the watering moment and green light after
   if (moisture_value >= 0 && moisture_value <= 550) {
+    digitalWrite(greenDiod, LOW);
+    digitalWrite(redDiod, HIGH);
     digitalWrite(relay, HIGH);
     delay(5000);
     digitalWrite(relay, LOW);
+    digitalWrite(redDiod, LOW);
+    digitalWrite(greenDiod, HIGH);
   }
   digitalWrite(sensorPower, LOW);
-  //time to the next moisture check
   delay(30000);
 }
