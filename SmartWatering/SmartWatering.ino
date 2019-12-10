240e9430c4c848209b8b87d940ca07df22eff44e
excitexcite <56977515+excitexcite@users.noreply.github.com>
2019-12-10 11:06:15 +0200

    Update SmartWatering.ino
    full project source code (release code)

--- SmartWatering/SmartWatering.ino
+++ SmartWatering/SmartWatering.ino
@@ -1,35 +1,71 @@
-//diod support test
-#define relay = 8;
-#define sensorPower = 2;
-#define sensorControl = A0;
-#define redDiod = 7;
-#define greenDiod = 13; 
-int moisture_value;
+//full program
+
+#define relay 8
+#define sensorPower 2
+#define sensorControl A0
+#define redDiod 7
+#define yellowDiod 12
+#define greenDiod 13  
+int veryWetBorder = 500;
+int semiWetBorder = 700;
+int veryDryBorder = 1023;
 
 void setup() {
+  // set up console output and pin mode
   Serial.begin(9600);
   pinMode(sensorPower, OUTPUT);
   pinMode(relay, OUTPUT);
   pinMode(redDiod, OUTPUT);
+  pinMode(yellowDiod, OUTPUT);
   pinMode(greenDiod, OUTPUT); 
 }
 
 void loop() {
   digitalWrite(sensorPower, HIGH);
-  //get moisture value
-  moisture_value = analogRead(sensorControl);
-  Serial.print(moisture_value);
-  Serial.print("\n");
-  //red light until the watering moment and green light after
-  if (moisture_value >= 0 && moisture_value <= 550) {
-    digitalWrite(greenDiod, LOW);
-    digitalWrite(redDiod, HIGH);
-    digitalWrite(relay, HIGH);
-    delay(5000);
-    digitalWrite(relay, LOW);
-    digitalWrite(redDiod, LOW);
-    digitalWrite(greenDiod, HIGH);
+  delay(100);
+
+  for (int i = 0; i < 3; i++) {
+    //get moisture value
+    moisture_value = analogRead(sensorControl);
+    //output moisutre value to serial console 
+    Serial.print(moisture_value);
+    Serial.print("\n");
+      if (setMoistureLevelDiod() == 1) {
+            digitalWrite(relay, HIGH);
+            //watering time
+            delay(2500);                 
+            digitalWrite(relay, LOW);
+            delay(15000);    
+       }
+      else 
+          break;
   }
+  
   digitalWrite(sensorPower, LOW);
-  delay(30000);
+  //time to the next moisture check in ms
+  // 1 hour
+  delay(600000);
+
+}
+
+// return 0 if the earth is dry and 1 if the earth is wet or semi-wet
+int setMoistureLevelDiod() {
+  if (moisture_value >= 0 && moisture_value < veryWetBorder) {
+    digitalWrite(greenDiod, HIGH);
+    digitalWrite(yellowDiod, LOW);
+    digitalWrite(greenDiod, LOW);
+    return 1;
+  } 
+  else if (moisture_value >= veryWetBorder && moisture_value < semiWetBorder) {
+    digitalWrite(yellowDiod, HIGH);
+    digitalWrite(redDiod, LOW);
+    digitalWrite(greenDiod, LOW);
+    return 1;
+  }
+  else if (moisture_value >= semiWetBorder && moisture_value < veryDryBorder) {
+     digitalWrite(redDiod, HIGH);
+     digitalWrite(yellowDiod, LOW);
+     digitalWrite(greenDiod, LOW);
+     return 0;
+  }
 }
